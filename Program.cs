7e1c28b199e0737e07d293a11082c1b46b067b6a
Taylor Seager <taylorseager718@gmail.com>
2024-07-15 10:23:03 -0500

    updated while loop to have an end

--- Program.cs
+++ Program.cs
@@ -1,27 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string greeting = @"Welcome to the Amazing Guessing Game!";
+string greeting = @"Welcome to the Amazing Guessing Game!
+You get 4 guesses! Choose wisely.";
 Console.WriteLine(greeting);
 
-int secretNumber = GenerateRandomNumber(1, 3);
-Console.WriteLine("Please try to guess the secret number:");
+int secretNumber = GenerateRandomNumber(1, 10);
 int userGuess;
 
 bool correctAnswerGuessed = false;
 int maxGuesses = 4;
 int guessCount = 0;
 
-if (!int.TryParse(Console.ReadLine().Trim(), out userGuess))
+while (!correctAnswerGuessed && guessCount < maxGuesses)
 {
-    Console.WriteLine("Please enter valid input");
-}
-else if (secretNumber == userGuess)
-{
-    Console.WriteLine("Congrats! You guessed the secret number!");
-}
-else
-{
-    Console.WriteLine("You lose. You guessed wrong!");
+    Console.WriteLine("Please try to guess the secret number:");
+
+    if (!int.TryParse(Console.ReadLine().Trim(), out userGuess))
+    {
+        Console.WriteLine("Please enter valid input");
+    }
+    else if (secretNumber == userGuess)
+    {
+        Console.WriteLine("Congrats! You guessed the secret number!");
+        correctAnswerGuessed = true;
+    }
+    else
+    {
+        Console.WriteLine("You lose. You guessed wrong!");
+    }
+
+    guessCount++;
 }
 
 static int GenerateRandomNumber(int min, int max)
